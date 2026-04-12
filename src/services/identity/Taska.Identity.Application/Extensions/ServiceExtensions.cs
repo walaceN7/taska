0f0e7962ff5c985bd54ac65a97e6f201a2eaf686
Walace Abreu <gbwalace@gmail.com>
2026-04-12 11:38:15 -0300

    feat(identity): add FluentValidation with pipeline behavior

--- src/services/identity/Taska.Identity.Application/Extensions/ServiceExtensions.cs
+++ src/services/identity/Taska.Identity.Application/Extensions/ServiceExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using Taska.Identity.Application.Behaviors;
+using Taska.Identity.Application.Validators;
 
 namespace Taska.Identity.Application.Extensions;
 
@@ -7,6 +11,8 @@ public static class ServiceExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediator(options => options.ServiceLifetime = ServiceLifetime.Scoped);
+        services.AddValidatorsFromAssembly(typeof(RegisterCommandValidator).Assembly);
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
         return services;
     }
---
+++ src/services/identity/Taska.Identity.Application/Validators/RegisterCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Taska.Identity.Application.Commands;
+
+namespace Taska.Identity.Application.Validators;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100).WithMessage("First name must not exceed 100 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(100).WithMessage("Last name must not exceed 100 characters");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Email is not valid");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches("[0-9]").WithMessage("Password must contain at least one number")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
+    }
+}
