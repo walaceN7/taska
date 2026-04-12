711d6b78f0f124cf4e82910688d4b9c4f89205ab
Walace Abreu <gbwalace@gmail.com>
2026-04-12 14:56:30 -0300

    feat: initialize identity service API project with Serilog, health checks, and middleware configuration

--- src/services/identity/Taska.Identity.API/Program.cs
+++ src/services/identity/Taska.Identity.API/Program.cs
@@ -1,3 +1,5 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 using Taska.Identity.API.Middlewares;
 using Taska.Identity.Application.Extensions;
@@ -19,6 +21,12 @@ builder.Host.UseSerilog((context, services, configuration) => configuration
     .ReadFrom.Services(services)
     .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"));
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection")!)
+    .AddProcessAllocatedMemoryHealthCheck(512, name: "memory", tags: new[] { "memory" })
+    .AddDiskStorageHealthCheck(options =>
+        options.AddDrive("C:\\", 1024), name: "disk", tags: new[] { "disk" });
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -32,5 +40,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseMiddleware<ExceptionMiddleware>();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
