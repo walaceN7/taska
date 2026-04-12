effef401f139ccc665dd8eec305f92fe4898ec1f
Walace Abreu <gbwalace@gmail.com>
2026-04-12 10:26:46 -0300

    feat(identity): add EF Core migrations and database configuration

---
+++ src/services/identity/Taska.Identity.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taska.Identity.Domain.Entities;
+using Taska.Identity.Infrastructure.Persistence;
+
+namespace Taska.Identity.Infrastructure.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TaskaIdentityDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddDataProtection();
+
+        services.AddIdentityCore<User>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = false;
+        })
+        .AddRoles<IdentityRole<Guid>>()
+        .AddEntityFrameworkStores<TaskaIdentityDbContext>()
+        .AddDefaultTokenProviders();
+
+        return services;
+    }
+}
